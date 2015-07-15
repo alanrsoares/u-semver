b9408cc7a37840c29f320593e4afcc37c5d8e8c0
Alan R. Soares <alanrsoars@gmail.com>
2015-07-16 11:14:32 +1200

    set up tests

---
+++ specs/u-semver.spec.js
@@ -0,0 +1,43 @@
+import { resolve } from '../src/u-semver';
+import { expect } from 'chai';
+
+const VERSIONS = [
+  '1.0.1',
+  '1.1.9',
+  '1.0.0',
+  '0.1.1-alpha',
+  '1.3.2',
+  '0.2.3-pre',
+  '0.2.4-pre',
+  '0.2.0',
+  '0.0.12',
+  '0.0.13-pre'
+];
+
+let runAssertion = ([input, expected, pre]) => {
+  let value = resolve(input, VERSIONS, pre);
+  it(`should resolve ${ input } to ${ expected }`, () => {
+    expect(value).to.equal(expected);
+  });
+};
+
+const cases = [
+//[input, expected, pre]
+  ['1.0.2', undefined],
+  ['1.0.0', '1.0.0'],
+  ['1.0.1', '1.0.1'],
+  ['1.3.2', '1.3.2'],
+  ['^1.0.0', '1.3.2'],
+  ['^0.1.0', '0.2.0'],
+  ['~1.0.0', '1.0.1'],
+  ['~0.1.0', '0.1.1-alpha', true],
+  ['^0.1.0', '0.2.4-pre', true],
+  ['^0.0.1', '0.2.4-pre', true],
+  ['~0.0.1', '0.0.12'],
+  ['~0.0.1', '0.0.13-pre', true],
+  ['latest', '1.3.2']
+];
+
+describe('Given a set of versions', () => {
+  cases.map(runAssertion);
+});
---
+++ src/u-semver.js
@@ -0,0 +1,30 @@
+let find = (xs, fn) => xs.filter(fn)[0];
+
+let findLatest = (xs) => xs.sort().reverse()[0];
+
+let findPattern = (xs, pattern) => {
+  const RX = new RegExp(pattern);
+  return findLatest(xs.filter(::RX.test));
+};
+
+export const resolve = (range, versions, pre) => {
+  if (range === 'latest') {
+    return findLatest(versions);
+  }
+
+  const VERSION_RX = /^([\^\~])?(\d+)\.(\d+)\.(\d+)(-(\w+))?$/;
+
+  let [root, prefix, major, minor] = VERSION_RX.exec(range);
+
+  if (!prefix) {
+    return find(versions, (v) => v === root);
+  }
+
+  let pattern = prefix === '^'
+    ? `^(${ major })\\.(\\d+)\\.(\\d+)`
+    : `^(${ major })\.(${ minor })\\.(\\d+)`;
+
+  return pre
+    ? findPattern(versions, pattern + '(-(\\w+))?$')
+    : findPattern(versions, pattern + '$');
+};
