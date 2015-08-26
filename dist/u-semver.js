67e3722c64fcb3d30bc6e9d9450beb885acc319e
Alan R. Soares <alanrsoars@gmail.com>
2015-08-26 16:15:03 +1200

    build dist

--- dist/u-semver.js
+++ dist/u-semver.js
@@ -7,9 +7,11 @@ Object.defineProperty(exports, '__esModule', {
 var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i['return']) _i['return'](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError('Invalid attempt to destructure non-iterable instance'); } }; })();
 
 var semVerToNum = function semVerToNum(x) {
-  return x.match(/(\d+).(\d+).(\d+)/).slice(1).reduce(function (acc, x, i) {
-    return +acc + +x * [100000, 1000, 1][i];
-  });
+  return x.match(/(\d+).(\d+).(\d+)/).slice(1).map(function (m) {
+    return +m;
+  }).reduce(function (acc, x, i) {
+    return acc + x * [1000000, 1000, 1][i];
+  }, 0);
 };
 
 var sortSemver = function sortSemver(a, b) {
