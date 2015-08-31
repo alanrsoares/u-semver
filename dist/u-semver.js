eb7c104b4e13a5ce1d2112f7edca0c58bb024b68
Alan R. Soares <alanrsoars@gmail.com>
2015-08-31 22:50:17 +1200

    build dist

--- dist/u-semver.js
+++ dist/u-semver.js
@@ -6,16 +6,29 @@ Object.defineProperty(exports, '__esModule', {
 
 var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i['return']) _i['return'](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError('Invalid attempt to destructure non-iterable instance'); } }; })();
 
+var VERSION_RX = /^([\^\~])?(\d+)\.(\d+)\.(\d+)(-(\w+)(\.(\d+))?)?$/;
 var multipliers = [1000000, 1000, 10, 0, 1];
 
-var semVerToNum = function semVerToNum(x) {
-  return x.match(/^(\d+)\.(\d+)\.(\d+)(-(\w+)(\.(\d+))?)?$/).slice(1).map(function (m) {
+var toValidIntMatches = function toValidIntMatches(x) {
+  return x.match(VERSION_RX).slice(1).map(function (m) {
     return +m;
   }).filter(function (m) {
     return !isNaN(m);
-  }).reduce(function (acc, y, i) {
+  });
+};
+
+var semVerToNum = function semVerToNum(x) {
+  return toValidIntMatches(x).reduce(function (acc, y, i) {
     return acc + (y + 1) * multipliers[i];
   }, 0);
+};
+
+var filterVersion = function filterVersion(filters) {
+  return function (x) {
+    return toValidIntMatches(x).reduce(function (acc, y, i) {
+      return acc && (filters[i] !== undefined ? y >= filters[i] : true);
+    }, true);
+  };
 };
 
 var sortSemver = function sortSemver(a, b) {
@@ -33,20 +46,20 @@ var find = function find(xs, fn) {
   return xs.filter(fn)[0];
 };
 
-var isPreRelease = function isPreRelease(x) {
+var allowsPreRelease = function allowsPreRelease(x) {
   return x && x.indexOf('-') >= 0;
 };
 
 var findLatest = function findLatest(xs) {
   var latest = xs.sort(sortSemver).reverse()[0];
-  return isPreRelease(latest) ? find(xs, function (x) {
+  return allowsPreRelease(latest) ? find(xs, function (x) {
     return x === latest.split('-')[0];
   }) || latest : latest;
 };
 
-var findPattern = function findPattern(xs, pattern) {
+var findPattern = function findPattern(xs, pattern, filters) {
   var RX = new RegExp(pattern);
-  return findLatest(xs.filter(RX.test.bind(RX)));
+  return findLatest(xs.filter(RX.test.bind(RX)).filter(filterVersion(filters)));
 };
 
 var resolve = function resolve(range, versions, pre) {
@@ -54,18 +67,20 @@ var resolve = function resolve(range, versions, pre) {
     return findLatest(versions);
   }
 
-  var VERSION_RX = /^([\^\~])?(\d+)\.(\d+)\.(\d+)(-(\w+)(\.(\d+))?)?$/;
-
   var _VERSION_RX$exec = VERSION_RX.exec(range);
 
-  var _VERSION_RX$exec2 = _slicedToArray(_VERSION_RX$exec, 4);
+  var _VERSION_RX$exec2 = _slicedToArray(_VERSION_RX$exec, 7);
 
   var root = _VERSION_RX$exec2[0];
   var prefix = _VERSION_RX$exec2[1];
   var major = _VERSION_RX$exec2[2];
   var minor = _VERSION_RX$exec2[3];
+  var patch = _VERSION_RX$exec2[4];
+  var partial = _VERSION_RX$exec2[5];
+  var beta = _VERSION_RX$exec2[6];
 
   if (!prefix) {
+    // match exact value
     return find(versions, function (v) {
       return v === root;
     });
@@ -73,7 +88,9 @@ var resolve = function resolve(range, versions, pre) {
 
   var pattern = prefix === '^' ? '^(' + major + ')\\.(\\d+)\\.(\\d+)' : '^(' + major + ').(' + minor + ')\\.(\\d+)';
 
-  return pre ? findPattern(versions, pattern + '(-(\\w+)(\\.(\\d+))?)?$') : findPattern(versions, pattern + '$');
+  var filters = [major, minor, patch, partial, beta];
+
+  return pre ? findPattern(versions, pattern + '(-(\\w+)(\\.(\\d+))?)?$', filters) : findPattern(versions, pattern + '$', filters);
 };
 
 var SemVer = { resolve: resolve };
