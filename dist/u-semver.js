23cb0609845a55bcda771323da354946474b10ad
Alan R. Soares <alanrsoars@gmail.com>
2016-04-01 15:31:35 +1300

    improved and simplified sort algorhitm

--- dist/u-semver.js
+++ dist/u-semver.js
@@ -6,11 +6,28 @@ Object.defineProperty(exports, '__esModule', {
 
 var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i['return']) _i['return'](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError('Invalid attempt to destructure non-iterable instance'); } }; })();
 
-var VERSION_RX = /^([\^\~])?(\d+)\.(\d+)\.(\d+)(-(\w+)(\.(\d+))?)?$/;
-var multipliers = [1000000, 1000, 10, 0, 1];
+var _context;
+
+var SEMVER_RX = /^([\^\~])?(\d+)\.(\d+)\.(\d+)(-(\w+)(\.(\d+))?)?$/;
+var MULTIPLIERS = [1000000, 1000, 10, 0, 1];
+
+var find = function find(xs, fn) {
+  return xs.filter(fn)[0];
+};
+var last = function last(xs) {
+  return xs[xs.length - 1];
+};
+var contains = function contains(x, s) {
+  return x.indexOf(s) > -1;
+};
+var and = function and(f, xs) {
+  return xs.reduce(function (acc, x) {
+    return acc && f(x);
+  }, true);
+};
 
 var toValidIntMatches = function toValidIntMatches(x) {
-  return x.match(VERSION_RX).slice(1).map(function (m) {
+  return x.match(SEMVER_RX).slice(1).map(function (m) {
     return +m;
   }).filter(function (m) {
     return !isNaN(m);
@@ -25,24 +42,23 @@ var filterVersion = function filterVersion(filters) {
   };
 };
 
-var find = function find(xs, fn) {
-  return xs.filter(fn)[0];
+var isPreRelease = (_context = /(alpha|beta)/).test.bind(_context);
+var arePreReleases = function arePreReleases(xs) {
+  return and(isPreRelease, xs);
 };
-
 var allowsPreRelease = function allowsPreRelease(x) {
   return x && x.indexOf('-') >= 0;
 };
 
 function semVerToNum(x) {
-  var match = toValidIntMatches(x);
-  var acc = 0;
-  match.forEach(function (y, i) {
-    return acc += (y + 1) * multipliers[i];
-  });
-  return acc;
+  var matches = toValidIntMatches(x);
+  var reducer = function reducer(acc, y, i) {
+    return acc += y * MULTIPLIERS[i];
+  };
+  return matches.reduce(reducer, 0);
 }
 
-function sortSemver(a, b) {
+function sortSemVer(a, b) {
   var _map = [a, b].map(semVerToNum);
 
   var _map2 = _slicedToArray(_map, 2);
@@ -50,11 +66,25 @@ function sortSemver(a, b) {
   var a1 = _map2[0];
   var b1 = _map2[1];
 
-  return a1 === b1 ? 0 : a1 < b1 ? -1 : 1;
+  if (arePreReleases([a, b])) {
+    var _map3 = [a, b].map(getBaseSemVer);
+
+    var _map32 = _slicedToArray(_map3, 2);
+
+    var a2 = _map32[0];
+    var b2 = _map32[1];
+
+    if (a2 === b2 && (contains(a, 'beta') && contains(b, 'alpha'))) {
+      return 1;
+    }
+  }
+
+  return a1 - b1;
 }
 
 function findLatest(xs) {
-  var latest = xs.sort(sortSemver).reverse()[0];
+  var sorted = xs.sort(sortSemVer);
+  var latest = last(sorted);
   return allowsPreRelease(latest) ? find(xs, function (x) {
     return x === latest.split('-')[0];
   }) || latest : latest;
@@ -70,17 +100,17 @@ function resolve(range, versions, pre) {
     return findLatest(versions);
   }
 
-  var _VERSION_RX$exec = VERSION_RX.exec(range);
+  var _SEMVER_RX$exec = SEMVER_RX.exec(range);
 
-  var _VERSION_RX$exec2 = _slicedToArray(_VERSION_RX$exec, 7);
+  var _SEMVER_RX$exec2 = _slicedToArray(_SEMVER_RX$exec, 7);
 
-  var root = _VERSION_RX$exec2[0];
-  var prefix = _VERSION_RX$exec2[1];
-  var major = _VERSION_RX$exec2[2];
-  var minor = _VERSION_RX$exec2[3];
-  var patch = _VERSION_RX$exec2[4];
-  var partial = _VERSION_RX$exec2[5];
-  var beta = _VERSION_RX$exec2[6];
+  var root = _SEMVER_RX$exec2[0];
+  var prefix = _SEMVER_RX$exec2[1];
+  var major = _SEMVER_RX$exec2[2];
+  var minor = _SEMVER_RX$exec2[3];
+  var patch = _SEMVER_RX$exec2[4];
+  var partial = _SEMVER_RX$exec2[5];
+  var beta = _SEMVER_RX$exec2[6];
 
   if (!prefix) {
     // match exact value
--- src/u-semver.js
+++ src/u-semver.js
@@ -1,8 +1,13 @@
-const VERSION_RX = /^([\^\~])?(\d+)\.(\d+)\.(\d+)(-(\w+)(\.(\d+))?)?$/;
-const multipliers = [1000000, 1000, 10, 0, 1];
+const SEMVER_RX = /^([\^\~])?(\d+)\.(\d+)\.(\d+)(-(\w+)(\.(\d+))?)?$/;
+const MULTIPLIERS = [1000000, 1000, 10, 0, 1];
+
+const find = (xs, fn) => xs.filter(fn)[0];
+const last = (xs) => xs[xs.length - 1];
+const contains = (x, s) => x.indexOf(s) > -1;
+const and = (f, xs) => xs.reduce((acc, x) => acc && f(x), true);
 
 const toValidIntMatches = (x) =>
-  x.match(VERSION_RX)
+  x.match(SEMVER_RX)
    .slice(1)
    .map(m => +m)
    .filter(m => !isNaN(m));
@@ -12,25 +17,32 @@ const filterVersion = (filters) => (x) =>
     .reduce((acc, y, i) =>
       acc && (filters[i] !== undefined ? y >= filters[i] : true), true);
 
-const find = (xs, fn) => xs.filter(fn)[0];
-
+const isPreRelease = ::/(alpha|beta)/.test;
+const arePreReleases = (xs) => and(isPreRelease, xs);
 const allowsPreRelease = (x) => x && x.indexOf('-') >= 0;
 
-function semVerToNum(x) {
-  const match = toValidIntMatches(x);
-  let acc = 0;
-  match.forEach((y, i) => acc += (y + 1) * multipliers[i]);
-  return acc;
+function semVerToNum (x) {
+  const matches = toValidIntMatches(x);
+  const reducer = (acc, y, i) => acc += y * MULTIPLIERS[i];
+  return matches.reduce(reducer, 0);
 }
 
-function sortSemver(a, b) {
+function sortSemVer (a, b) {
   const [a1, b1] = [a, b].map(semVerToNum);
-  return a1 === b1
-    ? 0 : a1 < b1 ? -1 : 1;
+
+  if (arePreReleases([a, b])) {
+    const [a2, b2] = [a, b].map(getBaseSemVer);
+    if (a2 === b2 && (contains(a, 'beta') && contains(b, 'alpha'))) {
+      return 1;
+    }
+  }
+
+  return a1 - b1;
 }
 
 function findLatest(xs) {
-  const latest = xs.sort(sortSemver).reverse()[0];
+  const sorted = xs.sort(sortSemVer);
+  const latest = last(sorted);
   return allowsPreRelease(latest)
     ? find(xs, (x) => x === (latest.split('-')[0])) || latest
     : latest;
@@ -46,7 +58,7 @@ function resolve(range, versions, pre) {
     return findLatest(versions);
   }
 
-  const [root, prefix, major, minor, patch, partial, beta] = VERSION_RX.exec(range);
+  const [root, prefix, major, minor, patch, partial, beta] = SEMVER_RX.exec(range);
 
   if (!prefix) {
     // match exact value
